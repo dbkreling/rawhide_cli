5329d51d614b253ed5c0b73e1cffc0935966973e
Daniel Kreling <dkreling@redhat.com>
2025-02-17 00:27:20 +0100

    Add create_packages_list() for later contents comparison
    With two lists, we will be able to compare its contents and evaluate which ones
    were added, removed or updated.

--- cli.py
+++ cli.py
@@ -18,6 +18,21 @@ def create_json_file(date, filename):
                         file.write(chunk)
     return local_filename
 
+def create_packages_list(filename):
+    packages_list = []
+    try:
+        with open(filename, "r", encoding="utf-8") as file:
+            data = json.load(file)
+            packages = data["payload"]["rpms"]["Everything"]["x86_64"]
+            for pack in packages:
+                packages_list.append(pack)
+    except FileNotFoundError:
+        print(f"Error: File '{filename}' not found.")
+    except Exception as e:
+        print(f"Error: {e}")
+
+    return packages_list
+
 def main():
     parser = argparse.ArgumentParser(description="A simple CLI example.")
     parser.add_argument("-i", "--initdate", type=str, help="The date to download the initial json file." )
@@ -27,18 +42,9 @@ def main():
     init_file = create_json_file(args.initdate, f"rpms_{args.initdate}.json")
     final_file = create_json_file(args.finaldate, f"rpms_{args.finaldate}.json")
 
-    try:
-        with open(args.filename, "r", encoding="utf-8") as file:
-            data = json.load(file)
-            # print(data)
-        with open("output.json", "w", encoding="utf-8") as fileo:
-            json.dump(data, fileo, indent=4)
-    except FileNotFoundError:
-        print(f"Error: File '{args.filename}' not found.")
-    except Exception as e:
-        print(f"Error: {e}")
+    init_packages = create_packages_list(init_file)
+    final_packages = create_packages_list(final_file)
 
-    print(f"Output file output.json created.")
 
 if __name__ == "__main__":
     main()
