215c041b8b8ecb7645f64092dd10cfd0937ba455
Daniel Kreling <dkreling@redhat.com>
2025-02-16 15:02:18 +0100

    Add argparse to include commands and parameters to the cli
    Adding argparse and a main() function with commands:
    
    - 'last': displays the updates in the last X days (to be implemented)
    - 'calc': calculates date X days before today

--- print_rawhide_composes.py
+++ print_rawhide_composes.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import requests
 from bs4 import BeautifulSoup
 from collections import Counter
@@ -27,12 +28,35 @@ dates.pop(-1)
 counter = Counter(dates)
 
 # print(dates)
-
+# print()
 # print(counter)
-print("Here's a list of Rawhide updates per day:" )
-for key,value in counter.items():
-    if value == 1:
-        singular_plural = "update"
+
+def print_all_updates():
+def calculate_past_date(days):
+    return
+
+def number_of_updates_since_date(args_days):
+    return
+
+def main():
+    parser = argparse.ArgumentParser(description="Calculate the date X days before today.")
+
+    subparsers = parser.add_subparsers(dest='command', help='Sub-command help')
+
+    add_parser = subparsers.add_parser('last', help="Updates in the last X days")
+    add_parser.add_argument('days', type=int, help="First number")
+    add_parser.set_defaults(func=lambda args: number_of_updates_since_date(args.days))
+
+    add_parser = subparsers.add_parser('calc', help='Calculate date X days before today')
+    add_parser.add_argument('days', type=int, help='Number of days to find the date')
+    add_parser.set_defaults(func=lambda args: calculate_past_date(args.days))
+
+    args = parser.parse_args()
+
+    if hasattr(args, 'func'):
+        args.func(args)
     else:
-        singular_plural = "updates"
-    print(f"{value} {singular_plural} on {key}")
+        parser.print_help()
+
+if __name__ == "__main__":
+    main()
