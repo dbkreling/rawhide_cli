eed2da5c49574783e759be972b4fd1096caa2a17
Daniel Kreling <dkreling@redhat.com>
2025-02-16 23:11:14 +0100

    Implement function to download the initial and final json files
    Also remove unused (and conflicting) argument "--filename".

--- cli.py
+++ cli.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python3
 
 import argparse
+import requests
 import json
+
+def create_json_file(date, filename):
+    url = f'https://kojipkgs.fedoraproject.org/compose/rawhide/Fedora-Rawhide-{date}.n.0/compose/metadata/rpms.json'
+    local_filename = filename
+
+    with requests.get(url, stream=True) as response:
+        response.raise_for_status()  # Raises an HTTPError for bad responses (e.g., 404 or 500)
+        with open(local_filename, 'wb') as file:
+            # Write the file in chunks to avoid using too much memory
+            for chunk in response.iter_content(chunk_size=8192):
+                if chunk:  # Filter out keep-alive new chunks
+                    file.write(chunk)
+    return local_filename
 
 def main():
     parser = argparse.ArgumentParser(description="A simple CLI example.")
-    parser.add_argument("-f", "--filename", type=str, help="Name of the file to be parsed.", default="rpms.json")
+    parser.add_argument("-i", "--initdate", type=str, help="The date to download the initial json file." )
+    parser.add_argument("-f", "--finaldate", type=str, help="The date to download the final json file." )
     args = parser.parse_args()
-    
-    print(f"Parsing file.... {args.filename}")
+
+    create_json_file(args.initdate, f"rpms_{args.initdate}.json")
+    create_json_file(args.finaldate, f"rpms_{args.finaldate}.json")
 
     try:
         with open(args.filename, "r", encoding="utf-8") as file:
