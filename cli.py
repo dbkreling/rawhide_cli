ceaf1f773e638d44c5dd48a12ede64041bb6bca9
Daniel Kreling <dkreling@redhat.com>
2025-02-16 23:50:00 +0100

    Add logic to skip download if file already exists

--- cli.py
+++ cli.py
@@ -8,13 +8,14 @@ def create_json_file(date, filename):
     url = f'https://kojipkgs.fedoraproject.org/compose/rawhide/Fedora-Rawhide-{date}.n.0/compose/metadata/rpms.json'
     local_filename = filename
 
-    with requests.get(url, stream=True) as response:
-        response.raise_for_status()  # Raises an HTTPError for bad responses (e.g., 404 or 500)
-        with open(local_filename, 'wb') as file:
-            # Write the file in chunks to avoid using too much memory
-            for chunk in response.iter_content(chunk_size=8192):
-                if chunk:  # Filter out keep-alive new chunks
-                    file.write(chunk)
+    if not local_filename:
+        with requests.get(url, stream=True) as response:
+            response.raise_for_status()  # Raises an HTTPError for bad responses (e.g., 404 or 500)
+            with open(local_filename, 'wb') as file:
+                # Write the file in chunks to avoid using too much memory
+                for chunk in response.iter_content(chunk_size=8192):
+                    if chunk:  # Filter out keep-alive new chunks
+                        file.write(chunk)
     return local_filename
 
 def main():
