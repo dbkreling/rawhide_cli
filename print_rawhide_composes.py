51b5c1dfd33ab18f34a0441b9350e447e57ab85c
Daniel Kreling <dkreling@redhat.com>
2025-02-16 11:29:47 +0100

    Add dates as LIST

--- print_rawhide_composes.py
+++ print_rawhide_composes.py
@@ -14,23 +14,15 @@ if response.status_code != 200:
 
 # Parse the HTML
 soup = BeautifulSoup(response.text, "html.parser")
-update_count_per_day=1
-update_date=0
-last_update_date=0
-
+dates = []
 # Extract links (assuming directory listings are hyperlinks)
 for link in soup.find_all("a"):
     href = link.get("href")
     if href and not href.startswith("?") and not href.startswith("/"):
-        update_date=(href.split("-", 3)[2]).split(".")[0]
+        date = (href.split("-", 3)[2]).split(".")[0]
+        dates.append(date)
 
-        if last_update_date == update_date:
-            update_count_per_day+=1
-        else:
-            update_count_per_day=1
+print(dates)
 
-        last_update_date=update_date
 
-        print(last_update_date, update_count_per_day)
-
-print(f"Date {update_date} had {update_count_per_day} updates")        # print(href)  # Print directory/file names
+        # print(href)  # Print directory/file names
